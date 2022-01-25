bde45f1cf1fdcac6b92f0c44718ddbc719b2f5a1
Juan Pestana <juan.pestana82@gmail.com>
2022-01-25 22:38:06 +0100

    fixing some property names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,40 @@
-import tw from 'twin.macro'
-import { Button, Logo, Navbar } from '../src/components/template'
+import React from 'react'
+import tw, { styled, css, theme } from 'twin.macro'
 
-const styles = {
-  // Move long class sets out of jsx to keep it scannable
-  container: ({ hasBackground }: { hasBackground: boolean }) => [
-    tw`flex flex-col items-center justify-center h-screen bg-blue-800`,
-    hasBackground && tw`bg-gradient-to-b from-electric to-ribbon`,
-  ],
+import Navbar from '../src/components/fementor/Navbar'
+import HeadOrange from '../src/components/fementor/HeadOrange'
+import Feature from '../src/components/fementor/Feature'
+import Products from '../src/components/fementor/Products'
+import Testimonials from '../src/components/fementor/Testimonials'
+import Decoration from '../src/components/fementor/Decoration'
+import Footer from '../src/components/fementor/Footer'
+import egg from '../src/images/desktop/image-transform.jpg'
+import cup from '../src/images/desktop/image-stand-out.jpg'
+
+const StBgImage = styled.div`
+  ${css`
+    background-image: url(https://res.cloudinary.com/hrbotfactory-com/image/upload/v1639251061/image-header_afgpb5.jpg);
+    background-position: center;
+    background-size: cover;
+  `};
+  ${tw`h-screen`}
+`
+
+function fementor() {
+  return (
+    <>
+      <StBgImage>
+        <Navbar />
+        <HeadOrange />
+      </StBgImage>
+      <Feature image={egg} />
+      <Feature image={cup} reverse={true} />
+      <Products />
+      <Testimonials />
+      <Decoration />
+      <Footer />
+    </>
+  )
 }
 
-const App = () => (
-  <>
-    <Navbar />
-    <div css={styles.container({ hasBackground: false })}>
-      <div tw="flex flex-col justify-center h-full gap-y-5">
-        <Button variant="primary">Submit</Button>
-        <Button variant="secondary">Cancel</Button>
-        <Button isSmall>Close</Button>
-      </div>
-      <Logo />
-    </div>
-  </>
-)
-
-export default App
+export default fementor
--- src/components/fementor/HeadOrange.tsx
+++ src/components/fementor/HeadOrange.tsx
@@ -25,11 +25,11 @@ function HeadOrange() {
       <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg">
         <g
           stroke="#FFF"
-          stroke-width="6"
+          strokeWidth="6"
           fill="none"
-          fill-rule="evenodd"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          fillRule="evenodd"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path d="M18 3v100M3 95.484l15 15 15-15" />
         </g>
